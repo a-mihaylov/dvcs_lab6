cfc6d2d14c348bcc85fa3bb8ccdfc4f264235dcd
a.s.mihaylov <a.s.mihaylov@yandex.ru>
2019-05-15 00:23:34 +0400

    13

--- aaa.cpp
+++ aaa.cpp
@@ -5,7 +5,7 @@ int c = 555;
 //delete line 5 branch third. conflict with commit 2
 int b2 = 55;
 int c2 = 555;
-int d2 = 5555;
+//delete line 8 branch third. conflict with commit 2
 //delete line 9 branch third. conflict with commit 3, 8
 int b3 = 55;
 int c3 = 555;
